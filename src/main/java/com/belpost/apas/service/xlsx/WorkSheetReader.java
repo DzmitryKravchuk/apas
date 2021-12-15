3b242deca1ba64a28639375bc3b23da5d71eeb5b
dzmitry.kravchuk <dzmitry.kravchuk@gmail.com>
2021-12-15 07:09:11 +0300

    worksheetReader

--- src/main/java/com/belpost/apas/service/xlsx/WorkSheetReader.java
+++ src/main/java/com/belpost/apas/service/xlsx/WorkSheetReader.java
@@ -1,22 +1,93 @@
 package com.belpost.apas.service.xlsx;
 
+import com.belpost.apas.service.util.CustomObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WorkSheetReader {
 
-    public String readCellValue(XSSFSheet workSheet, int rowIndex, int columnIndex) {
+    private final CustomObjectMapper objectMapper;
+
+    public <T> DataModel<T> read(XSSFSheet workSheet, Class<T> clazz) throws IOException {
+        log.info("Start reading from workSheet [{}]", workSheet.getSheetName());
+        DataModel<T> dataModel = new DataModel<>();
+        //process metadata
+        String metadataAsString = readCellValue(workSheet, 0, 0);
+        processMetadata(dataModel, metadataAsString);
+
+        //process header
+        final Map<Integer, String> header = getHeaderAsMap(workSheet);
+
+        //process content
+        fillContent(dataModel, workSheet, clazz, header);
+
+        log.info("Finish reading from workSheet [{}]", workSheet.getSheetName());
+
+        return dataModel;
+    }
+
+    private <T> void fillContent(DataModel<T> dataModel, XSSFSheet workSheet, Class<T> clazz, Map<Integer, String> header) throws IOException {
+        int lastRowIndex = workSheet.getLastRowNum();
+        for (int i = 2; i <= lastRowIndex; i++) {
+            XSSFRow nextRow = workSheet.getRow(i);
+            String json = convertRowToStringObject(header, nextRow);
+            final T pojo = objectMapper.readFromFile(json, clazz);
+            dataModel.getContent().add(pojo);
+        }
+    }
+
+    private String convertRowToStringObject(Map<Integer, String> header,
+                                            XSSFRow nextRow) throws JsonProcessingException {
+        Iterator<Cell> cellIter = nextRow.cellIterator();
+        Map<String, String> rowObjectMap = new HashMap<>();
+        while (cellIter.hasNext()) {
+            XSSFCell nextCell = (XSSFCell) cellIter.next();
+            rowObjectMap.put(header.get(nextCell.getColumnIndex()),
+                    readCellValue(nextCell));
+        }
+        return objectMapper.writeValueAsString(rowObjectMap);
+    }
+
+    private Map<Integer, String> getHeaderAsMap(XSSFSheet workSheet) {
+        final Map<Integer, String> header = new HashMap<>();
+        XSSFRow nextRow = workSheet.getRow(1);
+        Iterator<Cell> cellIter = nextRow.cellIterator();
+        while (cellIter.hasNext()) {
+            XSSFCell nextCell = (XSSFCell) cellIter.next();
+            header.put(nextCell.getColumnIndex(), readCellValue(nextCell));
+        }
+        return header;
+    }
+
+    private <T> void processMetadata(DataModel<T> dataModel, String metadataAsString) {
+        String[] keyVals = metadataAsString.split(",");
+        for (String keyVal : keyVals) {
+            String[] parts = keyVal.split(":", 2);
+            dataModel.getMetadata().put(parts[0], parts[1]);
+        }
+    }
+
+    private String readCellValue(XSSFSheet workSheet, int rowIndex, int columnIndex) {
         XSSFRow nextRow = workSheet.getRow(rowIndex);
         Cell nextCell = nextRow.getCell(columnIndex);
         return nextCell.getStringCellValue();
     }
 
-    public String readCellValue(Cell nextCell) {
+    private String readCellValue(Cell nextCell) {
         return nextCell.getStringCellValue();
     }
 
--- src/test/java/com/belpost/apas/service/xlsx/XlsxFileReaderTest.java
+++ src/test/java/com/belpost/apas/service/xlsx/XlsxFileReaderTest.java
@@ -3,13 +3,20 @@ package com.belpost.apas.service.xlsx;
 import com.belpost.apas.model.OfficeModel;
 import com.belpost.apas.service.util.CustomObjectMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 @SpringBootTest(classes = {XlsxFileReader.class,
-    ObjectMapper.class,
-    CustomObjectMapper.class})
+        FileInputStream.class,
+        ObjectMapper.class,
+        CustomObjectMapper.class})
 class XlsxFileReaderTest {
 
     private static final String officeFile = "src/test/resources/xlsx/office.xlsx";
@@ -17,11 +24,10 @@ class XlsxFileReaderTest {
     @Autowired
     private XlsxFileReader fileReader;
 
-    @Autowired
-    private CustomObjectMapper customObjectMapper;
 
     @Test
-    void shouldReadFromOfficesToDataModel(){
-         DataModel <OfficeModel> model;
+    void shouldReadFromOfficesToDataModel() throws IOException {
+        DataModel<OfficeModel> model = fileReader.read(new File(officeFile), OfficeModel.class);
+        Assertions.assertEquals(2, model.getContent().size());
     }
 }

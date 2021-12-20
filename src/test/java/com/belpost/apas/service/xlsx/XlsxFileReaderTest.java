1f15389c23e33f96dbb785728d3ccfb1fe0003f8
DzmitryKravchuk <dzmitry.kravchuk@gmail.com>
2021-12-20 15:49:06 +0300

    xlsx reader test - ok2

--- src/test/java/com/belpost/apas/service/xlsx/XlsxFileReaderTest.java
+++ src/test/java/com/belpost/apas/service/xlsx/XlsxFileReaderTest.java
@@ -1,18 +1,21 @@
 package com.belpost.apas.service.xlsx;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.belpost.apas.model.OfficeModel;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import org.junit.jupiter.api.Assertions;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {XlsxFileReader.class,
-        WorkSheetReader.class})
+    WorkSheetReader.class})
 class XlsxFileReaderTest {
 
     private static final String officeFile = "src/test/resources/xlsx/office.xlsx";
+    private static final Map<String, String> expectedMap = Map.of("INPUT", "officeData", "TYPE", "initial");
 
     @Autowired
     private XlsxFileReader fileReader;
@@ -23,6 +26,15 @@ class XlsxFileReaderTest {
         throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException,
         InstantiationException {
         DataModel<OfficeModel> model = fileReader.read(officeFile, OfficeModel.class);
-        Assertions.assertEquals(2, model.getContent().size());
+
+        assertThat(model.getMetadata())
+            .hasSize(2)
+            .isEqualTo(expectedMap);
+
+        assertThat(model.getContent())
+            .hasSize(2)
+            .satisfiesExactlyInAnyOrder(
+                o -> assertThat(o.getOfficeTypeCode()).isEqualTo("РайУч"),
+                o -> assertThat(o.getOfficeTypeCode()).isEqualTo("ОПС"));
     }
 }

35237a42f2567c08d28e08f2a2cc05e44e993c9d
dzmitry.kravchuk <dzmitry.kravchuk@gmail.com>
2021-12-22 07:01:05 +0300

    where in - broken query

--- src/test/java/com/belpost/apas/persistence/repository/OfficePersistenceTest.java
+++ src/test/java/com/belpost/apas/persistence/repository/OfficePersistenceTest.java
@@ -2,8 +2,6 @@ package com.belpost.apas.persistence.repository;
 
 import com.belpost.apas.exception.ResourceNotFoundException;
 import com.belpost.apas.persistence.entity.Office;
-import com.belpost.apas.persistence.repository.AbstractPersistenceTest;
-import com.belpost.apas.persistence.repository.OfficeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
@@ -33,6 +31,15 @@ class OfficePersistenceTest extends AbstractPersistenceTest {
         List<Office> l = repository.findAll();
 
         assertEquals(21, l.size());
+    }
+
+    @Test
+    void shouldGetAllOfficesByParentId() {
+        List<Office> l1 = repository.findAllByParentId(1L);
+        List<Office> l2 = repository.findAllByParentId(2L, 3L);
+
+        assertEquals(2, l1.size());
+        assertEquals(4, l2.size());
     }
 
 }
